f8504f892606fd0dce045f784ae038f470adee79
MonaRahmani <monarahmanimehr@gmail.com>
2020-09-18 15:25:43 -0700

    total_medals_per_team(olympic_data), completed

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -4,17 +4,25 @@ require 'awesome_print'
 def get_all_olympic_athletes(filename)
 
   athletes_arr_of_hash = CSV.read(filename, headers: true).map(&:to_h)
-  keys = athletes_arr_of_hash.map do |athlete|
+  filtered_keys = athletes_arr_of_hash.map do |athlete|
     athlete.select do |key, value|
       ["ID", "Name", "Height", "Team", "Year", "City", "Sport", "Event", "Medal"].include? key
     end
   end
-  return keys
+  return filtered_keys
 end
-
+# p get_all_olympic_athletes('../data/athlete_events.csv')
 def total_medals_per_team(olympic_data)
-
+  medal_hash = Hash.new(0)
+  olympic_data.each do |athlete|
+    if athlete["Medal"] != "NA"
+      medal_hash[athlete["Team"]] += 1
+    end
+  end
+  return medal_hash
 end
+
+# p total_medals_per_team(olympic_data)
 
 def get_all_gold_medalists(olympic_data)
 end
@@ -22,7 +30,7 @@ end
 
 #1-
 # -the get_all_olympic_athletes method get a file path and return array
-# of Olympic athletes hashes.
+# of Olympic athletes.
 # -Check that we get back an array
 # -Check that each element in the array is a hash
 
